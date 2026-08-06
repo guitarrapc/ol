6ac410bf00d5486c794f047fa3bb7e729157cd36
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2026-08-06 15:43:54 +0900

    chore: nuget mock to be container

--- sandbox/ecosystems/nuget/prepare.ps1
+++ sandbox/ecosystems/nuget/prepare.ps1
@@ -1,3 +1,6 @@
 $ErrorActionPreference = "Stop"
-dotnet build (Join-Path $PSScriptRoot "Ol.Ci.NuGet.csproj") -c Release
+docker build `
+    --target export `
+    --output "type=local,dest=$(Join-Path $PSScriptRoot "obj")" `
+    $PSScriptRoot
 if ($LASTEXITCODE -ne 0) { throw "NuGet dependency preparation failed." }
